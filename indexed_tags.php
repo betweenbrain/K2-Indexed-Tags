5b6fc01f6ecc5110ab50c7d7cddf04d46df33924
Matt Thomas <matt@betweenbrain.com>
2013-12-16 17:55:47 -0500

    Initial working version

---
+++ indexed_tags.php
@@ -0,0 +1,77 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * File       indexed_tags.php
+ * Created    12/16/13 2:30 PM
+ * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2013 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v3 or later
+ */
+
+// Load the K2 Plugin API
+JLoader::register('K2Plugin', JPATH_ADMINISTRATOR . '/components/com_k2/lib/k2plugin.php');
+
+// Instantiate class for K2 plugin events
+class plgK2Indexed_tags extends K2Plugin {
+
+	var $pluginName = 'indexed_tags';
+	var $pluginNameHumanReadable = 'K2 - Indexed Tags';
+
+	function plgK2Indexed_tags(& $subject, $results) {
+		parent::__construct($subject, $results);
+	}
+
+	/**
+	 * Function to Update item's extra_fields_search data with tag names
+	 *
+	 * @param $row
+	 * @param $isNew
+	 */
+	function onAfterK2Save(&$row, $isNew) {
+
+		$app = JFactory::getApplication();
+		$db  = JFactory::getDbo();
+
+		if ($app->isAdmin()) {
+
+			$tags     = $this->fetchTags($row->id);
+			$tagNames = null;
+
+			foreach ($tags as $tag) {
+				$tagNames .= $tag->name . ' ';
+			}
+
+			$query = 'UPDATE ' . $db->nameQuote('#__k2_items') . '
+				SET ' . $db->nameQuote('extra_fields_search') . ' = CONCAT(
+					' . $db->nameQuote('extra_fields_search') . ',' . $db->Quote($tagNames) . '
+				)
+				WHERE id = ' . $db->Quote($row->id) . '';
+			$db->setQuery($query);
+			$db->query();
+		}
+	}
+
+	/**
+	 * function to fetch an item's tags
+	 *
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
+	private function fetchTags($id) {
+
+		$db    = JFactory::getDbo();
+		$query = 'SELECT tag.name, tag.id
+			FROM ' . $db->nameQuote('#__k2_tags') . '  as tag
+			LEFT JOIN ' . $db->nameQuote('#__k2_tags_xref') . '
+			AS xref ON xref.tagID = tag.id
+			WHERE xref.itemID = ' . $db->Quote($id) . '
+			AND tag.published = 1';
+
+		$db->setQuery($query);
+		$tags = $db->loadObjectList();
+
+		return $tags;
+	}
+}
